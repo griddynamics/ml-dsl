9e851b885a00fe8db9988513db6b12b858eeeb99
Alex Rodin <arodin@griddynamics.com>
2020-07-21 19:55:07 +0400

    [ML-DSL] Readiness for publishing

--- notebooks/demo/scripts/text_tokenizer.py
+++ notebooks/demo/scripts/text_tokenizer.py
@@ -1,5 +1,21 @@
 # %py_load demo/scripts/text_tokenizer.py
 #!/usr/bin/python
+# Copyright (c) 2020 Grid Dynamics International, Inc. All Rights Reserved
+# http://www.griddynamics.com
+# Classification level: PUBLIC
+# Licensed under the Apache License, Version 2.0(the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Id:          ML-DSL
+# Project:     ML DSL
+# Description: DSL to configure and execute ML/DS pipelines
+
 from pyspark import SQLContext, SparkContext
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
